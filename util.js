022593f0c41678585a4d6297147ecf72aadbaaf4
Zachary Bennett <zbennett10@hotmail.com>
2017-03-19 09:57:43 -0700

    configuring module

--- util.js
+++ util.js
@@ -2,11 +2,9 @@
 
 //use this config to test function in node
 //modify this IIFE to recieve the window global and attach Ragnarok to window.document
-const Rok = (function(){
+(function(exports){
 
-    function Ragnarok() {};
-
-    Ragnarok.prototype.log = function(split, ...args) {
+    exports.log = function(split, ...args) {
         if(split === true) {
             if(args.length < 1) console.log(true);
             args.forEach(arg => console.log(arg));
@@ -15,16 +13,17 @@ const Rok = (function(){
         console.log(split, ...args);
     }
 
-    Ragnarok.prototype.getType = function(value) {
+    exports.getType = function(value) {
         if(typeof value === 'object') {
             if(value.length !== undefined) return 'array';
         }
         return typeof value;
     }
 
-    
+    Rok = exports;
+
+})({});
 
 
-    return new Ragnarok();
-})();
+Rok.log(true, "poop", "sauce", "poop");
 
