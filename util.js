58e5d7c9ffe958475c52bbf8986aa986bed511c7
Zachary Bennett <zbennett10@hotmail.com>
2017-03-20 23:18:09 -0700

    insert and mutableInsert

--- util.js
+++ util.js
@@ -20,8 +20,44 @@
         return typeof value;
     }
 
+    exports.insert = function(array, index, value) {
+        if(array === []) return [value];
+        return array.slice(0, index).concat([value].concat(array.slice(index)));
+    }
+
+    exports.mutableInsert = function(array, index, value) {
+        if(array === []) {
+            array.push(value);
+            return;
+        }
+        array.push(array[array.length-1]);
+        for(let i = array.length - 1; i > index; i--) {
+            array[i] = array[i-1];
+        }
+        array[index] = value;
+    }
+
+    //still working on this
+    exports.mutableZip = function(left, right) {
+        let larger, smaller;
+        if(left.length >= right.length) {
+            larger = left;
+            smaller = right;
+        } else {
+            larger = right;
+            smaller = left;
+        }
+        larger.forEach((value, index, array) => {
+                exports.mutableInsert(array, index, smaller[index]);
+            });
+        return larger;
+    }
+
     Rok = exports;
 
 })({});
+
+console.log(Rok.mutableZip([1,3,5], [2,4]));
+
 
 
