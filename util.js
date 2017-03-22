b16fc866e8fc3c33cb77fe7c206a0ffc8e5dfbb3
Zachary Bennett <zbennett10@hotmail.com>
2017-03-21 23:45:07 -0700

    improving dateFromISO

--- util.js
+++ util.js
@@ -50,16 +50,16 @@
 
     //date helpers-----------------------------------
     exports.dateFromISO = function(isoString) {
-        return new Date(...isoString.split(/\D+/));
+        if(exports.getType(isoString) === 'date') return isoString;
+        //regex credit goes to -- http://www.pelagodesign.com/blog/2009/05/20/iso-8601-date-validation-that-doesnt-suck/
+        if(/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/.test(isoString)) {
+            return new Date(...isoString.split(/\D+/));
+        }
+        throw new Error("Invalid format - Please pass an ISO string.");
+        return null;
     }
 
     Rok = exports;
 
 })({});
-
-// const arr1 = [1,3];
-// const arr2 = [2,4,5,6];
-// console.log(Rok.zip(arr1, arr2));
-
-console.log(Rok.getType(new Date()));
 
